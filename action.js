473c7b46c5aa2f76e4514646cbc663b893704dd6
Sébastien FAUVART <sebastien.fauvart@adeo.com>
2019-11-20 22:51:41 +0100

    feat: add approle auth method

--- action.js
+++ action.js
@@ -3,12 +3,40 @@ const command = require('@actions/core/lib/command');
 const got = require('got');
 
 async function exportSecrets() {
+    const _methods = ['approle', 'token'];
+
     const vaultUrl = core.getInput('url', { required: true });
-    const vaultToken = core.getInput('token', { required: true });
+    const vaultMethod = core.getInput('method', { required: true });
+    const vaultRoleId = core.getInput('roleId', { required: false });
+    const vaultSecretId = core.getInput('secretId', { required: false });
+    var vaultToken = core.getInput('token', { required: false });
     const vaultNamespace = core.getInput('namespace', { required: false });
 
     const secretsInput = core.getInput('secrets', { required: true });
     const secrets = parseSecretsInput(secretsInput);
+
+    if (!_methods.includes(vaultMethod)) {
+        throw Error(`Sorry, method ${vaultMethod} currently not implemented.`);
+    }
+
+    switch (vaultMethod) {
+        case 'approle':
+            core.debug('Try to retrieve Vault Token from approle')
+            var options = { headers: { }, json: true, body: { role_id: vaultRoleId, secret_id: vaultSecretId }, responseType: 'json' };
+            if (vaultNamespace != null){
+                options.headers["X-Vault-Namespace"] = vaultNamespace
+            }
+            const result = await got.post(`${vaultUrl}/v1/auth/approle/login`, options);
+            if (result && result.body && result.body.auth && result.body.auth.client_token) {
+                vaultToken = result.body.auth.client_token;
+                core.debug('✔ Vault Token has retrieved from approle')
+            } else {
+                throw Error(`No token was retrieved with the role_id and secret_id provided.`);
+            }
+            break;
+        default:
+            break;
+    }
 
     for (const secret of secrets) {
         const { secretPath, outputName, secretKey } = secret;
